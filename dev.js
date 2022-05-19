904f6501eccac3347233e548810321724c1322e3
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2022-05-19 00:54:54 -0300

    Desafio da Semana 2 do Bootcamp da Gama Academy
    Um simples aplicativo que faz a validação de CPF.

---
+++ dev.js
@@ -0,0 +1,17 @@
+//&& ||
+// Imprima false
+console.log( 2000 <= 1000 && 5 <= 5 );
+    
+    // Imprima true
+    console.log( true 
+     false && false );
+    
+    // Imprima false
+    console.log( 20 == '20' 
+    digite
+     false );
+    
+    // Imprima true
+    console.log( false 
+    digite
+     true );
---
+++ prova.js
@@ -0,0 +1,40 @@
+/*programa Teste{
+    inteiro numero = 1; 
+    inteiro soma = 0;
+
+    enquanto (numero < 5) { 
+        soma = soma + numero
+        numero = numero + 1
+}
+escreva(soma) 
+*/
+
+let numero =1;
+let soma = 0;
+
+while(numero <5){
+    soma = soma + numero;
+    numero = numero + 1;
+}
+console.log(soma);
+
+/*let a = 3;
+let b = 5;
+let c;
+
+c= a*b+5;
+a = a+8/2;
+b = 5*b*5;
+console.log(a);
+console.log(b);
+console.log(c);*/
+/*funcao calculos()
+
+INICIO
+declare a, b, c;
+a = 3;
+b = 5;
+c = a * b + 5; a = a + 8 / 2;
+	b = 5 * b * 5; escreva (a);
+escreva (b); escreva (c);
+FIM*/
